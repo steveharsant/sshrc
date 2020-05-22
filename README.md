50849e10700ddf9ca0a70bd80c5011c3dfc69bd7
Steve Harsant <stevenharsant@gmail.com>
2020-05-22 18:11:46 +0100

    first crack...

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # sshrc
+
 An simple ssh wrapper to copy rc files to /tmp and source them as the connection is established
---
+++ sshrc.sh
@@ -0,0 +1,58 @@
+#!/usr/bin/env bash
+#
+# sshrc
+# version: 0.1.0
+# Author: Steve Harsant
+
+# Set liniting rules
+# shellcheck disable=SC2059
+# shellcheck disable=SC2091
+# shellcheck disable=SC2162
+# shellcheck disable=SC2164
+
+# Enable debug messages
+enable_debug=0
+
+host=$1
+
+debug() {
+  if [[ $enable_debug == 1 ]]; then
+    printf "$1\n"
+  fi
+}
+
+script_location="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+files_list="${HOME}/.sshrc_files"
+
+if [[ -f "$files_list" ]]; then
+  debug ".sshrc_files file found in homepath."
+
+elif [[ -f "$script_location/.sshrc_files" ]]; then
+  while true; do
+    read -p "No .sshrc_files found in homepath. Would you like to install it? [y/n]" YN
+    case $YN in
+    [Yy]*) break ;;
+    [Nn]*) exit ;;
+    *) echo "Please answer yes or no" ;;
+    esac
+  done
+
+  debug "Installing .sshrc_files to homepath"
+  cp "$script_location/.sshrc_files" "$files_list"
+
+else
+  # If .sshrc_files is not found found anywhere, exit 1
+  printf "No .sshrc_files file found. Create one in your home path and try again. exit 1 \n"
+  exit 1
+fi
+
+files=$(cat "${HOME}/.sshrc_files")
+for file in $files; do
+  if [[ -f $file ]]; then
+    debug "Copying $file over scp to $host:/tmp/"
+    scp "$file" "$host":/tmp/
+  fi
+done
+
+debug "Initaiting ssh connection"
+ssh -t "$host" "bash --rcfile /tmp/.bashrc"
